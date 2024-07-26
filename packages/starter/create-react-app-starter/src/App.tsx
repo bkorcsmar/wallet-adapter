af401f2c7b52512e882dcadb271ff4137ef03466
bkorcsmar <korcsmarb629@gmail.com>
2024-07-26 23:46:37 +0200

    Implement sendSystemTransfer.

--- packages/starter/create-react-app-starter/src/App.tsx
+++ packages/starter/create-react-app-starter/src/App.tsx
@@ -1,3 +1,4 @@
+import { Buffer } from 'buffer';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import {
     ConnectionProvider,
@@ -13,14 +14,21 @@ import {
     PublicKey,
     Transaction,
     LAMPORTS_PER_SOL,
-    TransactionInstruction,
+    TransactionInstruction, SystemProgram, TransferParams,
 } from '@solana/web3.js';
 import React, { FC, ReactNode, useEffect, useMemo, useState } from 'react';
-import { Buffer } from 'buffer';
 import './styles.css';
 
 require('./App.css');
 require('@solana/wallet-adapter-react-ui/styles.css');
+
+declare global {
+    interface Window {
+        Buffer: typeof Buffer;
+    }
+}
+
+window.Buffer = Buffer;
 
 const App: FC = () => {
     return (
@@ -74,11 +82,15 @@ const sendTransferInstruction = async (wallet : WalletContextState, connection: 
         console.log("PublicKey: ", publicKey.toBase58());
         console.log("Connection", connection);
 //        const programId = new PublicKey('DVbC3AGo1MXCTqVGRL9kANVdG2RsyRb9dxBWK77FY14X'); -- devnet
-        const programId = new PublicKey('438sqaQZU2hho8qJ15qxyQ2Jrb92C16VG2Lxe1XReRYP'); //-- localnet
+        const programId = new PublicKey('J9qDbjQBHEFY5fPB3fXS8je7UFeS2xW5jrzDVwbXzQUV'); //-- localnet
 
         // const publicKey1 = new PublicKey("A1VF6gKKVfFoC84QgQMw8MF1qdNfcj4z31Hui5SQrcWw");
         // const publicKey1 = new PublicKey("8uoRHMqDfpS4RYXrb2tMkW1n4k1h3dVJkLtZ6z4mp7zL"); // devnet
         // const publicKey1 = new PublicKey("GvKiT3WgKTW5qToUBXnvvvuTAs5NQbVfDdpNyWUDGkXN"); // localnet
+
+        const buffer = Buffer.alloc(1 + 8); // 1 byte for the instruction index, 8 bytes for u64
+        buffer[0] = 2; // the index of the 'transfer' instruction
+        buffer.writeBigInt64LE(BigInt(amount * LAMPORTS_PER_SOL), 1); // the amount to transfer, written as a 64-bit integer
 
         let transferInstruction = new TransactionInstruction({
             keys: [
@@ -86,9 +98,9 @@ const sendTransferInstruction = async (wallet : WalletContextState, connection: 
                 {pubkey: new PublicKey(targetPublicKey), isSigner: false, isWritable: true}
             ],
             programId: programId,
-            data: Buffer.from([2 /* the index of the 'transfer' instruction in the instruction vector */, amount * LAMPORTS_PER_SOL]),
+            data: buffer,
         });
-
+        console.log("Amount: ",  amount * LAMPORTS_PER_SOL)
         let transaction = new Transaction();
         transaction.add(transferInstruction);
         transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
@@ -110,11 +122,41 @@ const sendTransferInstruction = async (wallet : WalletContextState, connection: 
     }
 };
 
+const sendSystemTransfer = async (wallet : WalletContextState, connection: Connection, targetPublicKey: string, amount: number) => {
+    try {
+        const publicKey = wallet.publicKey!;
+        console.log("PublicKey: ", publicKey.toBase58());
+        console.log("Connection", connection);
+        let lamportBigInt = BigInt(amount * LAMPORTS_PER_SOL);
+        const transferParams : TransferParams = {
+            fromPubkey: publicKey,
+            toPubkey: new PublicKey(targetPublicKey),
+            lamports: lamportBigInt
+        };
+        let transaction = new Transaction();
+        transaction.add(SystemProgram.transfer(transferParams));
+        transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+        transaction.feePayer = publicKey;
+
+        // Sign the transaction
+        if (wallet.signTransaction) {
+            let signedTransaction = await wallet.signTransaction(transaction);
+            // Send the transaction and get the transaction id
+            let txid = await connection.sendRawTransaction(
+                signedTransaction.serialize(),
+                { skipPreflight: false, preflightCommitment: "confirmed" }
+            );
+            console.log("Transaction sent:", txid);
+
+        }
+    } catch (err) {
+        console.log(err);
+    }
+};
 
 const Content: FC = () => {
     const [targetPublicKey, setTargetPublicKey] = useState('');
     const [amount, setAmount] = useState('');
-
 
     const handlePublicKeyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setTargetPublicKey(event.target.value);
@@ -136,21 +178,37 @@ const Content: FC = () => {
 
     return (
         <div className="App">
-            <input className="wallet-adapter-input" type="text" value={targetPublicKey} onChange={handlePublicKeyChange} placeholder="Public Key" style={{ width: '20%'}}/>
-            <input className="wallet-adapter-input" type="number" value={amount} onChange={handleAmountChange} placeholder="Amount in SOL" />
+            <input className="wallet-adapter-input" type="text" value={targetPublicKey} onChange={handlePublicKeyChange}
+                   placeholder="Public Key" style={{ width: '20%' }} />
+            <input className="wallet-adapter-input" type="number" value={amount} onChange={handleAmountChange}
+                   placeholder="Amount in SOL" />
             <button className="wallet-adapter-button wallet-adapter-button-trigger"
-                onClick={() => {
-                    if (wallet.publicKey && wallet.signTransaction) {
-                        sendTransferInstruction(wallet, connection, targetPublicKey, Number.parseFloat(amount))
-                    } else {
-                        console.log("publicKey: ", publicKey);
-                        console.log("signTransaction: ", signTransaction);
-                        console.log("Connection: ", connection);
-                    }
+                    onClick={() => {
+                        if (wallet.publicKey && wallet.signTransaction) {
+                            sendTransferInstruction(wallet, connection, targetPublicKey, Number.parseFloat(amount))
+                        } else {
+                            console.log("publicKey: ", publicKey);
+                            console.log("signTransaction: ", signTransaction);
+                            console.log("Connection: ", connection);
+                        }
 
-                }}
+                    }}
             >
                 Send Instruction
+            </button>
+            <button className="wallet-adapter-button wallet-adapter-button-trigger"
+                    onClick={() => {
+                        if (wallet.publicKey && wallet.signTransaction) {
+                            sendSystemTransfer(wallet, connection, targetPublicKey, Number.parseFloat(amount))
+                        } else {
+                            console.log("publicKey: ", publicKey);
+                            console.log("signTransaction: ", signTransaction);
+                            console.log("Connection: ", connection);
+                        }
+
+                    }}
+            >
+                Send Transfer
             </button>
             <WalletMultiButton />
         </div>
